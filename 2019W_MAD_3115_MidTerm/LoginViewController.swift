3e184e79e45efc034f62de1819892c2931913d9e
MacStudent <macstudent@126-imac14.local>
2019-03-20 18:47:13 -0400

    login completed

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class LoginViewController: UIViewController {
     var sd=UserDefaults.standard
     @IBOutlet weak var rememberme: UISwitch!
+    
     @IBOutlet weak var txtpass: UITextField!
     @IBOutlet weak var txtuid: UITextField!
     override func viewDidLoad() {
@@ -19,6 +20,21 @@ class LoginViewController: UIViewController {
     }
 
     @IBAction func btnlogin(_ sender: UIButton) {
+        
+        if  (txtuid.text=="samir" && txtpass.text=="123")
+        {
+            
+            let sp = UIStoryboard(name: "Main", bundle: nil)
+            let LionVC = sp.instantiateViewController(withIdentifier: "studententry") as! StudentEntryViewController
+            self.navigationController?.pushViewController(LionVC, animated: true)
+        }
+        else
+        {
+            let alert = UIAlertController(title: "Invalid", message: "Invalid Username and Password", preferredStyle: .alert)
+            let actionDefault = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(actionDefault)
+            self.present(alert, animated: true, completion: nil)
+        }
         
         
         
