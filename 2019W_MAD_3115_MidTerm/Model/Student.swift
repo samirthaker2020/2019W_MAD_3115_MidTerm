eb406f1f3951f4833eb2447510e39b6605321b18
MacStudent <macstudent@126-imac14.local>
2019-03-20 19:44:22 -0400

    work on calculation

--- 2019W_MAD_3115_MidTerm/Model/Student.swift
+++ 2019W_MAD_3115_MidTerm/Model/Student.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+
+class Student {
+    var sid:String
+    var sname:String
+    var gender:String
+    var bdate:String
+    var marks:[Float]
+    
+    init() {
+        self.sid=String()
+self.sname=String()
+        self.gender=String()
+        self.bdate=String()
+        self.bdate=String()
+        self.marks=[]
+    }
+    
+    init(sid:String,sname:String,gen:String,bdate:String,marks:Array<Float>) {
+        self.sid=sid
+        self.sname=sname
+        self.gender=gen
+        self.bdate=bdate
+        self.marks=marks
+    }
+    
+    
+    func displaydata()
+    {
+        print(sid)
+        print(sname)
+        print(gender)
+        print(bdate)
+        for i in marks
+        {
+            print(i)
+        }
+    }
+    
+}
--- 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
+++ 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
@@ -8,15 +8,77 @@
 
 import UIKit
 
-class StudentEntryViewController: UIViewController {
-
+class StudentEntryViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+  
+    
+    var gender:String?
+    @IBOutlet weak var txtid: UITextField!
+    var bdate1:String=""
+    @IBOutlet weak var txtname: UITextField!
+    var cname:String=""
+    @IBOutlet weak var m1: UITextField!
+    var coursedetail=["MADT", "MODT", "CSD", "WADT"]
+    @IBOutlet weak var m5: UITextField!
+    @IBOutlet weak var m4: UITextField!
+    @IBOutlet weak var m3: UITextField!
+    @IBOutlet weak var m2: UITextField!
+    @IBOutlet weak var txtemail: UITextField!
+    var m=[Float]
     override func viewDidLoad() {
         super.viewDidLoad()
-
+let s=Student()
         // Do any additional setup after loading the view.
     }
     
 
+    @IBAction func grnder(_ sender: UISegmentedControl) {
+        gender=String(sender.selectedSegmentIndex)
+    }
+    @IBAction func bdate(_ sender: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-YYYY"
+        let strDate = dateFormatter.string(from: sender.date)
+        bdate1=strDate
+        
+    }
+    
+    @IBOutlet weak var course: UIPickerView!
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+       if component==0
+       {
+       
+       }else{ return self.coursedetail.count}
+        return 0
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if component==1
+        {
+            return coursedetail[row]
+        }
+        return "NIl"
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView==course
+        {
+            course=self.course[course.selectedRow(inComponent: 1)]!
+        }
+    }
+    @IBAction func calculate(_ sender: Any) {
+        m.append(m1.text)
+        m.append(m1.text)
+        m.append(m1.text)
+        m.append(m1.text)
+        m.append(m1.text)
+        let s1=Student(sid:txtid.text!,sname:txtname.text!,gen:gender,bdate:bdate1,marks:Array<Float>)
+        
+    }
+    
     /*
     // MARK: - Navigation
 
